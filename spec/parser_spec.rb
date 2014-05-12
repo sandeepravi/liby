00812cd7867e34d228e4817d92140f0c7faa9717
sandeepravi <sandeepravi.bits@gmail.com>
2014-05-12 14:44:57 +0530

    test cases for arithmetic operations

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -7,16 +7,51 @@ describe 'Parser' do
 
   describe 'Arithmetic' do
 
-    before do
-      @parsed = execute('(+ 3 3 3)')
-    end
-
     context 'when numbers are added' do
+
+      before do
+        @parsed = execute('(+ 3 3 3)')
+      end
 
       it "should return the result" do
         expect(@parsed).to eq(9)
       end
     end
+
+    context 'when numbers are subtracted' do
+
+      before do
+        @parsed = execute('(- 10 5)')
+      end
+
+      it "should return the result" do
+        expect(@parsed).to eq(5)
+      end
+    end
+
   end
+
+  context 'when numbers are multiplied' do
+
+    before do
+      @parsed = execute('(* 3 2 3)')
+    end
+
+    it "should return the result" do
+      expect(@parsed).to eq(18)
+    end
+  end
+
+  context 'when numbers are divided' do
+
+    before do
+      @parsed = execute('(/ 27 3)')
+    end
+
+    it "should return the result" do
+      expect(@parsed).to eq(9)
+    end
+  end
+
 
 end
